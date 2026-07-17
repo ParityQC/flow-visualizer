b5c77b1f8af4b1ad9389c565d5c1c12c7c483cd3
Reinhard Stahn <rainij36@proton.me>
2026-07-17 16:23:50 +0200

    Add BSD-3-Clause license (#5)
    Resolves #2. Resolves #3.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// ParityQC © 2026. See the LICENSE file in the top level directory for details.
 import { CircuitBuilder } from './components/CircuitBuilder';
 import { emptyCircuit } from './utils/exampleCircuits';
 import { DisplaySettingsProvider } from './utils/DisplaySettings';
